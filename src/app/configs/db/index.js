022d33c28be78bf8c159cac924bf731a9735682a
Nguyen Khac Khanh <encacap@gmail.com>
2020-08-09 08:13:18 +0700

    Change DB Connect string

--- src/app/configs/db/index.js
+++ src/app/configs/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connect = async () => {
     try {
-        await mongoose.connect(process.env.DB_CONNECT, {
+        await mongoose.connect(process.env.MONGODB_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
